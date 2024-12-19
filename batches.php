d5c803fd7f10670a43bd0de8fa30426d381b230d
R.K. Owen <dr.rk.owen@gmail.com>
2024-12-18 21:54:46 -0700

    For batch updates handle when no batches exist.

--- batches.php
+++ batches.php
@@ -10,6 +10,10 @@
 	include_once "3x5_db.inc";
 	include_once "one_batch.inc";
 
+	if (isset($_GET["errmsg"])) {
+		$errmsg = $_GET["errmsg"];
+	}
+
 	$onebid = new OneBatch();
 	$one_batch__ = $onebid->get_one_batch();
 
@@ -18,13 +22,17 @@
 
 	$url = "new_batch.php?batch_select";
 	if ( isset($_POST["submit"] )) {
-		$bname = $db->one_row(
+		if (empty($one_batch__)) {
+			$errmsg = "No Batch Selected";
+		} else {
+			$bname = $db->one_row(
 "SELECT batch,batch_help FROM i3x5_batch ".
 "WHERE uid={$user->uid} AND bid='$one_batch__'");
-		header("Location: $url=Update&bid=$one_batch__&name=".
-			urlencode($bname['batch'])
-			."&name_help=".urlencode($bname['batch_help']));
-		return;
+			header("Location: $url=Update&bid=$one_batch__&name=".
+				urlencode($bname['batch'])
+				."&name_help=".urlencode($bname['batch_help']));
+			return;
+		}
 	}
 
 	$thelp = sendhelp("{$user->project} - batch properties",
@@ -32,6 +40,7 @@
 	card_head("{$user->project} - batches");
 print "<!--{-->".form($_SERVER['PHP_SELF'],
 	table(row(head($thelp))
+	.(isset($errmsg)?row(head(warn($errmsg))):"")
 	.row(head(
 	"<!--{-->".table(row(cell("Update").cell($onebid->string_one_batch()))
 	.row(head(
